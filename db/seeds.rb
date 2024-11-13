e794646ba3b3e44ad713848b7f09ef0a07547af6
aderemi <73750355+gabrieladeremi@users.noreply.github.com>
2024-11-13 12:17:21 +0000

    seeding data into the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Clearing database'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Seeding data into the database'
+5.times do
+  restaurant = Restaurant.create({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample
+  })
+
+  puts 'creating a restuarant'
+
+  Review.create({
+    content: Faker::Restaurant.description,
+    rating: rand(1..5),
+    restaurant: restaurant
+  })
+
+  puts 'creating a review'
+end
